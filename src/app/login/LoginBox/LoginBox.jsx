30c6047975067011f299544eaca241fdc28233ef
Georges David <georgessdavid@gmail.com>
2025-07-28 11:51:03 -0300

    added login box

--- src/app/login/LoginBox/LoginBox.jsx
+++ src/app/login/LoginBox/LoginBox.jsx
@@ -1,9 +1,9 @@
 'use client';
-import { TextField, InputAdornment, IconButton } from '@mui/material';
+import { TextField, InputAdornment, IconButton, Button } from '@mui/material';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import { LoadingButton } from '@mui/lab';
 import { useState } from 'react';
+import Link from 'next/link';
 
 
 export default function LoginBox() {
@@ -12,9 +12,9 @@ export default function LoginBox() {
         setShowPassword(!showPassword);
     };
     return (
-        <div className="w-[31.25rem] max-h-[500px] bg-[rgba(255,255,255,0.15)] backdrop-blur-[1rem] flex flex-col m-[5rem]">
+        <div className="w-[31.25rem] bg-[rgba(255,255,255,0.15)] backdrop-blur-[1rem] flex flex-col m-[5rem]">
             <h2 className="text-2xl font-semibold mb-4 text-center m-[1.5rem] color-[var(--titles-foreground)]">Iniciar Sesión</h2>
-            <form className="flex flex-col items-center gap-4 p-4">
+            <form className="flex flex-col items-center gap-[1.5rem] p-4">
                 <TextField
                     label="Correo Electrónico"
                     variant="standard"
@@ -48,10 +48,18 @@ export default function LoginBox() {
                     }}
                     autoComplete="current-password"
                 />
-                <LoadingButton variant="contained" color="primary" type='submit' className="py-[2rem] w-36 text-center" loading={false} >
+                <Button variant="contained" color="primary" type='submit' className="w-36 text-center" loading={false} >
                     Iniciar Sesión
-                </LoadingButton>
+                </Button>
             </form>
+            <div className="flex justify-center items-center gap-2 mb-4">
+                <Link
+                    href="/forgot-password"
+                    className="color-[var(--titles-foreground)] hover:text-white transition-all"
+                >
+                    ¿OLVIDASTE TU CONTRASEÑA?
+                </Link>
+            </div>
         </div>
     )
 }
